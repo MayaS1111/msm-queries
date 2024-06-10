57b4cce5a5c257405eb371fb35d6d61f1b6316be
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-10 19:22:04 +0000

    routes and controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "directorspage" })
+  get("/directors/youngest", { :controller => "misc", :action => "youngestpage" })
+  get("/directors/eldest", { :controller => "misc", :action => "eldestpage" })
+  get("/movies", { :controller => "misc", :action => "moviespage" })
+  get("/actors", { :controller => "misc", :action => "actorspage" })
+
 end
