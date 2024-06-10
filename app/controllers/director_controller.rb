1a60858c596dad2f61d305036cb47add84a4ef1f
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-10 21:59:50 +0000

    detail page done

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -3,7 +3,11 @@ class DirectorController < ApplicationController
     render({ :template => "director_templates/directors"})
   end
   def detailpage
-    @the_id = params.fetch("the_id")
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({:id => the_id})
+    @the_director = matching_records.at(0)
+
     render({ :template => "director_templates/detail"})
   end
   def youngestpage
