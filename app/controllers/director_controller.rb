fc5f15956d2cc7f1e969b0bfed0fa769ff8fa33a
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-10 20:31:37 +0000

    added details route and page

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -2,6 +2,10 @@ class DirectorController < ApplicationController
   def directorpage
     render({ :template => "director_templates/directors"})
   end
+  def detailpage
+    @the_id = params.fetch("the_id")
+    render({ :template => "director_templates/detail"})
+  end
   def youngestpage
     render({ :template => "director_templates/youngest"})
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "director", :action => "directorpage" })
+  get("/directors/:the_id", { :controller => "director", :action => "detailpage" })
   get("/directors/youngest", { :controller => "director", :action => "youngestpage" })
   get("/directors/eldest", { :controller => "director", :action => "eldestpage" })
   get("/movies", { :controller => "movie", :action => "moviepage" })
