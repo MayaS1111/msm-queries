ea74b57d912951d014ea5a92c17c89dced6e25c6
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-11 17:31:11 +0000

    working pn actor details

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -2,4 +2,18 @@ class ActorController < ApplicationController
   def actorpage
     render({ :template => "actor_templates/actors"})
   end
+  def detailpage
+    the_id = params.fetch("the_id")
+
+    matching_records = Actor.where({:id => the_id})
+    @the_actor = matching_records.at(0)
+
+    matching_character = Character.where({:id => @the_actor.id})
+    @the_character = matching_character.at(0)
+
+    # matching_records = Director.where({:id => @the_movie.director_id})
+    # @the_director = matching_records.at(0)
+
+    render({ :template => "actor_templates/detail"})
+  end
 end
