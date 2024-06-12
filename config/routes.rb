47bedbbc3828cf8323025d01d449fcb40fde3871
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-12 14:04:18 +0000

    fixed youngest order problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "director", :action => "directorpage" })
-  get("/directors/:the_id", { :controller => "director", :action => "detailpage" })
   get("/directors/youngest", { :controller => "director", :action => "youngestpage" })
   get("/directors/eldest", { :controller => "director", :action => "eldestpage" })
+  get("/directors/:the_id", { :controller => "director", :action => "detailpage" })
   get("/movies", { :controller => "movie", :action => "moviepage" })
   get("/movies/:the_id", { :controller => "movie", :action => "detailpage" })
   get("/actors", { :controller => "actor", :action => "actorpage" })
